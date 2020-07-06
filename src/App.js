befb658b0e171000e1cf1312299057fa70b5549d
Bhrugesh Vadhankar <vbhrugesh123@gmail.com>
2020-07-06 16:13:15 +0530

    Working with change event handler with element key

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ class App extends Component {
   state = {
     persons: [
       {
+        id: "lmck",
         name: "Test User1",
         age: 21
       },
       {
+        id: "lmck2",
         name: "Test User2",
         age: 23
       },
       {
+        id: "lmck3",
         name: "Test User3",
         age: 22
       },
@@ -44,27 +47,44 @@ class App extends Component {
     });
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {
-          name: "Test User1",
-          age: 21
-        },
-        {
-          name: event.target.value,
-          age: 23
-        },
-        {
-          name: "Test User3",
-          age: 30
-        },
-      ]
-    });
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+		return p.id === id;
+	});
+	
+	const person = {
+		...this.state.persons[personIndex]
+	};
+
+	// const person = Object.assign({}, this.state.persons[personIndex]);
+
+	person.name = event.target.value;
+	const persons = [...this.state.persons];
+	persons[personIndex] = person;
+
+	this.setState( { persons: persons });
+
+    // this.setState({
+    //   persons: [
+    //     {
+    //       name: "Test User1",
+    //       age: 21
+    //     },
+    //     {
+    //       name: event.target.value,
+    //       age: 23
+    //     },
+    //     {
+    //       name: "Test User3",
+    //       age: 30
+    //     },
+    //   ]
+    // });
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({
       persons: persons
@@ -96,7 +116,9 @@ class App extends Component {
           return <Person
               name={ person.name } 
               age={ person.age }
-              click={ () => this.deletePersonHandler(index) }></Person>;
+              click={ () => this.deletePersonHandler(index) }
+              key = { person.id }
+              changed = { (event) =>  this.nameChangedHandler(event, person.id) }></Person>;
         }) }
       </div>
       );
@@ -104,7 +126,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -117,7 +139,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
         <button
           style={ style }
           onClick={ this.togglePersonsHandler }>Toggle Persons</button>
